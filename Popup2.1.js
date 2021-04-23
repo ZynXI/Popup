8f664b2e924ce983f1182868bb8a52f7f53a2305
ZynX <Zynx@live.fr>
2021-04-23 16:41:52 +0200

    Update and rename Popup6.js to Popup2.1.js

--- Popup2.1.js
+++ Popup2.1.js
@@ -1,29 +1,30 @@
 // Get the modal
-var modalpopup = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    
+// Get the button close
+var popupclose = document.getElementById("popupclose");
 
 // Get the <button> element that closes the modal
 var button = document.getElementsByClassName("sib-form-block__button-with-loader")[0];
 
-	
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modalpopup.style.display = "none";
-}
 
 // When the user clicks on the button, close the modal
 button.onclick = function() {
-  modalpopup.style.display = "none";
+  modal.style.display = "none";
+}
+
+// When the user clicks on the button, close the modal
+popupclose.onclick = function() {
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modalpopup) {
-    modalpopup.style.display = "none";
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
+    
