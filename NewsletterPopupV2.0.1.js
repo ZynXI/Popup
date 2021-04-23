f31a6d0f2d9d66321576950eb94ee1d35cf9b3c4
ZynX <Zynx@live.fr>
2021-04-23 14:36:34 +0200

    Update and rename NewsletterPopupV2.8.js to NewsletterPopupV2.0.1.js

--- NewsletterPopupV2.0.1.js
+++ NewsletterPopupV2.0.1.js
@@ -8,6 +8,7 @@ divPopUp.style.position = 'relative';
 divPopUp.style.zIndex = '999';
 document.body.appendChild(divPopUp);
 setTimeout(window.onload = () =>{jQuery("#popup-newsletter").load("https://cdn.jsdelivr.net/gh/ZynXI/Popup@main/PopUp2.7.html");}, 3000);
+sessionStorage.x = 1
 } else {
   
 }
