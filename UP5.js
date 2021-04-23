0722e35c32b12a7b1bc2f1b290e09932cdc6bb33
ZynX <Zynx@live.fr>
2021-04-23 15:30:14 +0200

    Update and rename UP4.js to UP5.js

---
+++ UP5.js
@@ -0,0 +1,15 @@
+window.onload = () =>{
+
+if (sessionStorage.x != 1) {
+let divPopUp = document.createElement('div');
+divPopUp.id = 'popup-newsletter';
+divPopUp.innerHTML = '';
+divPopUp.style.position = 'relative';
+divPopUp.style.zIndex = '999';
+document.body.appendChild(divPopUp);
+setTimeout(window.onload = () =>{jQuery("#popup-newsletter").load("https://cdn.jsdelivr.net/gh/ZynXI/Popup@main/UP2.html");}, 3000);
+} else {
+  
+}
+  
+}
