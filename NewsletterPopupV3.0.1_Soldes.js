d3496c29d1db158741228a61530a7cb2be198cfd
ZynX <Zynx@live.fr>
2021-06-29 18:35:36 +0200

    Add files via upload
    soldes summer 21

---
+++ NewsletterPopupV3.0.1_Soldes.js
@@ -0,0 +1,16 @@
+window.onload = () =>{
+
+if (sessionStorage.x != 1) {
+let divPopUp = document.createElement('div');
+divPopUp.id = 'popup-newsletter';
+divPopUp.innerHTML = '';
+divPopUp.style.position = 'relative';
+divPopUp.style.zIndex = '999';
+document.body.appendChild(divPopUp);
+setTimeout(window.onload = () =>{jQuery("#popup-newsletter").load("https://cdn.jsdelivr.net/gh/ZynXI/Popup@main/PopUp3.1.html");}, 3000);
+sessionStorage.x = 1
+} else {
+  
+}
+  
+}
